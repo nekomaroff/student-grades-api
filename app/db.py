37f645d2cb367140ac57f85f2fb4bdbfda4c0823
sl <nekomaroff@yandex.ru>
2026-01-30 15:51:01 +0300

    Move secrets to env variables and add dotenv support

--- app/db.py
+++ app/db.py
@@ -1,12 +1,16 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_CONFIG = {
-    "dbname": "grades_db",
-    "user": "grades_user",
-    "password": "grades_password",
-    "host": "localhost",
-    "port": 5432,
+    "dbname": os.getenv("POSTGRES_DB"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": int(os.getenv("POSTGRES_PORT", 5432)),
 }
 
 
